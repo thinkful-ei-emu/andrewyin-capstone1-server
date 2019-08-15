1205797f961294842822233e783eab18ab4fc76b
Andrew <andrewyin1994@gmail.com>
2019-08-15 11:42:18 -0700

    updated config for heroku database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || '8080',
   NODE_ENV: process.env.NODE_ENV || 'developement',
-  DB_URL: process.env.DB_URL || 'postgresql://dungeon-master@localhost/dungeons-and-dragons',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://dungeon-master@localhost/dungeons-and-dragons',
   JWT_SECRET: 'totally_secure_secret'
 };
